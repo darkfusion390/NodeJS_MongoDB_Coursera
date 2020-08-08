2dc59e41593c8cf70de527ed09c0fc0a7f98d318
darkfusion390 <pranavpanchumarthi5@gmail.com>
2020-08-08 09:49:31 -0400

    express generator

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,6 +12,25 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
 app.use(bodyparser.json());
+app.use('/dishes', dishRouter);
+
+// app.get('/dishes/:dishId', (req,res,next) => {
+//     res.end('Will send the details of the dish: '+ req.params.dishId + ' to you!');
+// });
+
+// app.post('/dishes/:dishId', (req,res,next) => {
+//     res.statusCode = 403;
+//     res.end('POST operation not supported on dishes');
+// });
+
+// app.put('/dishes/:dishId', (req,res,next) => {
+//     res.write('Updating the dish: '+ req.params.dishId + '\n');
+//     res.end('Will update the dish: '+ req.body.name + " with details: "+ req.body.description);
+// });
+
+// app.delete('/dishes/:dishId', (req,res,next) => {
+//     res.end('Deleting the dish: '+ req.params.dishId);
+// });
 
 app.use((req,res,next) => {
     // console.log(req.headers);
